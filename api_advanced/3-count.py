a1c696d13f63b9cb6c8230e8a495365eb4b0c2f3
aadedigbae <134280190+aadedigbae@users.noreply.github.com>
2023-11-12 20:50:58 +0100

    Update 3-count.py 6

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -8,12 +8,11 @@ import json
 import requests
 
 
-def count_words(subreddit, word_list, after="", count=[]):
+def count_words(subreddit, word_list, after="", count=None):
     """Function to count_words
     """
-
-    if after == "":
-        count = [0] * len(word_list)
+    if count is None:
+        count = {}
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url,
@@ -24,35 +23,27 @@ def count_words(subreddit, word_list, after="", count=[]):
     if request.status_code == 200:
         data = request.json()
 
-        for topic in (data['data']['children']):
+        for topic in data['data']['children']:
             for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
+                clean_word = ''.join(char for char in word if char.isalnum()).lower()
+                if clean_word in word_list:
+                    if clean_word not in count:
+                        count[clean_word] = 1
+                    else:
+                        count[clean_word] += 1
 
         after = data['data']['after']
         if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
-
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
+            print_results(count)
         else:
             count_words(subreddit, word_list, after, count)
+
+
+def print_results(count):
+    sorted_counts = sorted(count.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print("{}: {}".format(word, count))
+
+
+# Example usage:
+count_words('unpopular', ['you', 'unpopular', 'vote', 'down', 'downvote', 'her', 'politics'])
